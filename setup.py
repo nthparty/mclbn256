eae5c0f7e579f8577c5e1136bded9f8703a1dcbe
Wyatt Howe <whowe@magnite.com>
2022-06-13 22:56:13 -0400

    Expand setup.py to fix naming bug
    Sadly, setuptools has no way to specify automatic platform name detection in setup.cfg - only setup.py.

--- setup.py
+++ setup.py
@@ -1,1 +1,7 @@
-import setuptools as s;s.setup()
+from setuptools import setup, Distribution
+
+class BinaryDistribution(Distribution):
+	# required for setuptools to autodetect the right naming
+	has_ext_modules = lambda _ : True
+
+setup(distclass=BinaryDistribution)
