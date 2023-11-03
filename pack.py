4b945d223ed3c380a4e234d85c1537355a64cb69
Wyatt Howe <whowe@magnite.com>
2023-11-03 13:57:01 -0700

    Test macOS wheel building

---
+++ pack.py
@@ -0,0 +1,92 @@
+import fnmatch
+from os import makedirs, path, listdir, walk, remove
+from os.path import abspath
+from shutil import rmtree, copytree
+from subprocess import check_call
+from sys import executable
+from zipfile import ZipFile
+
+import pystache
+
+from truckle import build_wheel
+
+PYPI_PACKAGE_NAME = 'mclbn256'
+
+BINARY_IMPORTS = [
+    """import platform
+import pkg_resources
+
+def load_library(path_name):
+    if platform.system() == 'Windows':
+        lib_ext = ".dll"
+    elif platform.system() == 'Darwin':
+        lib_ext = ".dylib"
+    else:
+        lib_ext = ".so"
+    return cdll.LoadLibrary(pkg_resources.resource_filename('mclbn256', path_name+lib_ext))""",
+]
+
+IMPORT_PATCHES = [
+    'mclbn256.tmpl',
+]
+
+#
+# Find the specific platform tags on PyPI for the project you are working with.
+#
+plattag = 'macosx_12_0_universal2' # macOS universal bundle (x86/AMD+M1/M2/ARM 64-bit)
+
+
+build_dir = path.join('.', 'build', plattag)
+original_src_dir = path.join('.', PYPI_PACKAGE_NAME)
+if path.exists(build_dir):
+    rmtree(build_dir)
+makedirs(build_dir)
+src_dir = path.join(build_dir, PYPI_PACKAGE_NAME)
+copytree(original_src_dir, src_dir)
+
+print(*listdir(src_dir))
+
+binaries = [path.join(dp, f) for dp, dn, fn in walk(src_dir) for f in fn
+            if ('.pyd' in f) or ('.dll' in f) or ('.so' in f) or ('.dylib' in f)]
+
+# assert len(binaries) == 1 and "Support for multiple binaries (rare edge case) is not yet implemented."
+
+bss = []
+for binary_path in map(path.join, binaries):
+
+    print('binary_path =', binary_path)
+
+    f = open(binary_path, 'rb')
+    bss.append(f.read())
+    f.close()
+    remove(binary_path)  # Either remove the already-portably-bundled binaries here, or have truckle ignore them.
+
+assert len(BINARY_IMPORTS) == 1 and "Support for multiple imports (edge case) is not yet implemented."
+assert len(IMPORT_PATCHES) == 1 and "Support for multiple patches (edge case) is not yet implemented."
+
+#data = {'hex://'+path.relpath(binaries[0], start=src_dir): bs.hex() for i, bs, filepath in enumerate(zip(bss, binary_path))}
+data = {'BINARY_HEX_'+str(i+1): bs.hex() for i, bs in enumerate(bss)}#{'BINARY_HEX': bs.hex()}
+template = open(IMPORT_PATCHES[0], encoding='utf-8').read()
+patched_code = pystache.render(template, data)
+
+def findReplace(directory, find, replace, filePattern):
+    # Citation: https://stackoverflow.com/a/6257321
+    for parentpath, dirs, files in walk(abspath(directory)):
+        for filename in fnmatch.filter(files, filePattern):
+            filepath = path.join(parentpath, filename)
+            with open(filepath) as f:
+                s = f.read()
+            s = s.replace(find, replace)
+            with open(filepath, "w") as f:
+                f.write(s)
+
+# Inject rendered source patch into src directory file(s).
+findReplace(src_dir, BINARY_IMPORTS[0], patched_code, "*.py")
+
+build_wheel(path.join('.', 'setup.cfg'), platform_tag=plattag)
+
+
+
+
+# plattag = 'macosx_11_0_arm64'
+# build_wheel('/Users/whowe/Downloads/datacable-1.1.0-------macosx_11_0_arm64', platform_tag=plattag)
---
+++ truckle.py
@@ -0,0 +1,226 @@
+"""
+A simple setuptools alternative for packing Python FFI bindings into wheels.
+"""
+from __future__ import annotations
+
+
+# import datacable
+
+
+
+
+
+
+
+
+
+import base64
+import glob
+import hashlib
+import os
+import sys
+import zipfile
+from typing import Union
+import doctest
+import toml, configparser
+from packaging.tags import platform_tags#, parse_tag
+
+def build_wheel(pyproject_path: str, wheel_file_name: str = None, platform_tag: str = None) -> str:
+    # pylint: disable=C0301 # Accommodates long link URLs.
+    """
+    Build a wheel.
+
+    Example.
+
+    >>> 1 in [1]
+    True
+    """
+
+    plattag = platform_tag or next(platform_tags()) or 'py3-none-any'  # or os.path.basename(project_root).split('-')[-1]
+
+    infofile = 'setup.cfg' if 'setup.cfg' in pyproject_path else ('pyproject.toml' if 'pyproject.toml' in pyproject_path else None)
+    #
+    project_root = pyproject_path[:-len(infofile)]
+    # project_root = os.path.abspath(extracted_wheel_path)
+    #version_guess = [x for x in os.listdir(os.path.join(project_root, os.path.pardir)) if os.path.basename(project_root)+'-' in x and '.dist-info' in x][0][len(os.path.basename(project_root)+'-'):-len('.dist-info')]  # Usually this line would be unnecessary.
+    #
+    # with open(pyproject_path, 'r') as fd:
+    #     pyproject = toml.loads(fd.read())
+    #     fd.close()
+    # with open(os.path.join(project_root, '-'.join(os.path.basename(project_root).split('-')[:2]) + '.dist-info', 'METADATA'), 'r') as fd:
+    # with open(os.path.join(project_root, os.path.basename(project_root) + '-' + version_guess + '.dist-info', 'METADATA'), 'r') as fd:
+    # with open(os.path.join(project_root, [x for x in os.listdir(project_root) if '.dist-info' in x][0], 'METADATA'), 'r') as fd:
+    #     # pyproject = toml.loads(fd.read())
+    #     pyproject = Distribution()
+    #     pyproject['parse'](fd.read())
+    #     fd.close()
+    # print(pyproject)
+
+    config = configparser.ConfigParser()
+    config.read(infofile)
+    pyproject = config['metadata']
+    assert pyproject['name'] == 'mclbn256'
+
+
+    pyproject['new_name'] = pyproject['name']#'csvtables'#pyproject['name'] + '_bundleable'
+    pyproject['old_name'] = pyproject['name']
+    pyproject['name'] = pyproject['new_name']
+
+    pyproject['new_version'] = '1.3.3'#pyproject['version']
+    pyproject['old_version'] = pyproject['version']
+    pyproject['version'] = pyproject['new_version']
+
+    # pyproject = Distribution()
+    # pyproject['parse'](m)
+    # print(vars(pyproject))
+
+
+    # {distribution}-{version}(-{build tag})?-{python tag}-{abi tag}-{platform tag}.whl
+    wheel_file_name = wheel_file_name or F"{pyproject['name']}-" \
+                                         F"{pyproject['version']}-" \
+                                         F"py3-" \
+                                         F"none-" \
+                                         F"{plattag}" \
+                                         F".whl"
+
+    if os.path.isdir(os.path.join(project_root, pyproject['old_name'])):
+        module_root = os.path.join(project_root, pyproject['old_name'])
+    elif os.path.isdir(os.path.join(project_root, 'src', pyproject['old_name'])):
+        module_root = os.path.join(project_root, 'src', pyproject['old_name'])
+    else:
+        raise ModuleNotFoundError("Cannot find module source!")
+
+    old_info_root = F"{pyproject['old_name']}-{pyproject['old_version']}.dist-info"
+    info_root = F"{pyproject['name']}-{pyproject['new_version']}.dist-info"
+
+    # with open(project_root + pyproject['project']['readme'], 'r') as fd:
+    #     readme = fd.read()
+    #     fd.close()
+    readme = pyproject['description']
+
+    # metadata = ('METADATA', F"Metadata-Version: 2.1\n" \
+    #                         F"Name: {pyproject['project']['name']}\n" \
+    #                         F"Version: {pyproject['project']['version']}\n" \
+    #                         F"Summary: {pyproject['project']['description']}\n" \
+    #                         F"Home-page: {pyproject['project']['urls']['Repository']}\n" \
+    #                         F"Author: {pyproject['project']['authors'][0]['name']}\n" \
+    #                         F"Author-email: {pyproject['project']['authors'][1]['email']}\n" \
+    #                         F"Project-URL: Bug Tracker, {pyproject['project']['urls']['Repository']}"
+    #                         F"/issues\n" \
+    #                         F"Classifier: Programming Language :: Python :: 3\n" \
+    #                         F"Classifier: Operating System :: OS Independent\n" \
+    #                         F"Requires-Python: {pyproject['project']['requires-python']}\n" \
+    #                         F"Description-Content-Type: text/x-rst\n" \
+    #                         F"License-File: LICENSE\n\n{readme}".encode('ascii', 'ignore'))
+    metadata = ('METADATA', F"Metadata-Version: 2.1\n" \
+                            F"Name: {pyproject['name']}\n" \
+                            F"Version: {pyproject['version']}\n" \
+                            # F"Summary: {pyproject['summary']}\n" \
+                            F"Home-page: {pyproject['url']}\n" \
+                            F"Author: {pyproject['author']}\n" \
+                            F"Author-email: {pyproject['author_email']}\n" \
+                            # F"Project-URL: Bug Tracker, {pyproject['project']['urls']['Repository']}"
+                            F"/issues\n" \
+                            # F"License: {pyproject['license']}\n" \
+                            F"Classifier: Programming Language :: Python :: 3\n" \
+                            F"Classifier: Operating System :: OS Independent\n" \
+                            # F"Requires-Python: {pyproject['requires_python']}\n" \
+                            F"Description-Content-Type: text/x-rst\n" \
+                            F"License-File: LICENSE\n\n{readme}".encode('ascii', 'ignore'))
+    # F"Classifier: License :: OSI Approved :: MIT License\n" \
+
+
+
+    license = ('LICENSE', (
+        lambda fd: (
+            lambda read:
+            fd.close() or read
+        )(fd.read())
+    )(open(os.path.join(project_root, 'LICENSE'), 'rb')))
+
+    wheel = ('WHEEL', F"Wheel-Version: 1.0\n" \
+                      F"Generator: truckle (0.1.x)\n" \
+                      F"Root-Is-Purelib: false\n".encode('ascii') +
+                     b"\n".join(F"Tag: {tag}".encode('ascii') for tag in plattag.split('.')) + b"\n\n")
+
+    print(wheel[1].decode())
+
+    toplevel = ('top_level.txt', F"{pyproject['name']}\n".encode())
+
+    record = ('RECORD', b'')
+
+    pushed_directory = os.getcwd()  # Does Python need push/popd functionality?
+    os.chdir(project_root)
+    module_file_paths = glob.glob(os.path.join(module_root, '**', '*'), recursive=True)
+    os.chdir(module_root)
+    module_files = list(map(os.path.relpath,
+                            glob.glob(os.path.join(module_root, '**', '*'), recursive=True)))
+
+    module_data = [
+        (filepath, open(filepath, 'rb').read())
+        for filepath
+        in module_files if os.path.isfile(filepath) and '__pycache__' not in filepath
+    ]
+
+    record = ('RECORD', '\n'.join(
+        (lambda parent_dir: os.path.join(parent_dir, filename))(
+            pyproject['name'] if (i >= 5) else info_root  # 4 == len([m, w, tl, r, l])
+        ) + ',sha256='
+        + base64.urlsafe_b64encode(hashlib.sha256(contents).digest()).rstrip(b"=").decode()
+        + ',' + str(len(contents))
+        for i, (filename, contents)
+        in reversed(list(enumerate(
+            list(reversed([metadata, wheel, toplevel, record, license]))
+            + module_data
+        )))
+    ))
+
+    # # print(metadata)
+    # # print(wheel)
+    # # print(toplevel)
+    # print(record)
+    # print()
+
+
+
+    module_files_not_pycache = [
+        filepath
+        for filepath
+        in module_files if os.path.isfile(filepath) and '__pycache__' not in filepath
+    ]
+
+    files = [
+            filepath
+            for filepath
+            in module_file_paths if os.path.isfile(filepath) and '__pycache__' not in filepath
+        ]
+
+    zf = zipfile.ZipFile(os.path.join(project_root, wheel_file_name), 'w')
+
+    for file_relpath, file_path in zip(module_files_not_pycache, files):
+        # print(file_path, os.path.join(pyproject['name'], file_relpath))
+        zf.write(file_path, os.path.join(pyproject['name'], file_relpath))
+        print(os.path.join(pyproject['name'], file_relpath))
+
+    for file_relpath, file_contents in [metadata, wheel, toplevel, record, license]:
+        # # print(len(file_contents), os.path.join(info_root, file_relpath))
+        # # zf.writestr(file_contents, os.path.join(info_root, file_relpath))
+        # print(os.path.join(info_root, file_relpath), len(file_contents))
+        zf.writestr(os.path.join(info_root, file_relpath), file_contents)
+        print(os.path.join(info_root, file_relpath))
+    #
+    # zf.close()
+    #
+    # zf = zipfile.ZipFile(os.path.join(project_root, wheel_file_name), 'r')
+    #
+    # for i in zf.infolist():
+    #     print(f"is_dir: {i.is_dir()}; filename: {i.filename}")
+    #
+    # zf.close()
+    #
+    #
+    print(F"`{pyproject['name']}` v{pyproject['version']} wheel built at {os.path.join(project_root, wheel_file_name)}\n")
+
+    os.chdir(pushed_directory)
+
+    return os.path.join(project_root, wheel_file_name)
